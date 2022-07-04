f0acac80ed23ea6ae09ba7e01f8eb9d0f340e08b
Massuer <95684438+Massuer@users.noreply.github.com>
2022-07-04 15:10:00 +0800

    Update ERC1155Tradable.sol

--- ERC1155Tradable.sol
+++ ERC1155Tradable.sol
@@ -19,3 +19,32 @@ contract ProxyRegistry {
  */
 contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
   using Strings for string;
+
+  address proxyRegistryAddress;
+  uint256 private _currentTokenID = 0;
+  mapping (uint256 => address) public creators;
+  mapping (uint256 => uint256) public tokenSupply;
+  // Contract name
+  string public name;
+  // Contract symbol
+  string public symbol;
+
+  /**
+   * @dev Require msg.sender to be the creator of the token id
+   */
+  modifier creatorOnly(uint256 _id) {
+    require(creators[_id] == msg.sender, "ERC1155Tradable#creatorOnly: ONLY_CREATOR_ALLOWED");
+    _;
+  }
+
+  /**
+   * @dev Require msg.sender to own more than 0 of the token id
+   */
+  modifier ownersOnly(uint256 _id) {
+    require(balances[msg.sender][_id] > 0, "ERC1155Tradable#ownersOnly: ONLY_OWNERS_ALLOWED");
+    _;
+  }
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
